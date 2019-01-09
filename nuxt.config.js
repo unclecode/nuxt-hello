961b3b0a6788c88aed7a14ce5887f344ed4b9692
unclecode <unclecode@kidocode.com>
2019-01-09 22:45:12 +0800

    html lang

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,74 +1,80 @@
 const pkg = require("./package");
 
 module.exports = {
-  mode: "universal",
+    mode: "universal",
 
-  generate: {
-    routes: [
-      '/posts/1',
-      '/posts/2',
-      '/posts/3'
-    ]
-  },
+    generate: {
+        routes: ["/posts/1", "/posts/2", "/posts/3"]
+    },
 
-  /*
-   ** Headers of the page
-   */
-  head: {
-    title: pkg.name,
-    meta: [
-      { charset: "utf-8" },
-      { name: "viewport", content: "width=device-width, initial-scale=1" },
-      { hid: "description", name: "description", content: pkg.description }
-    ],
-    link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }]
-  },
-
-  server: {
-    port: process.env.PORT || 8000, // default: 3000
-    host: "0.0.0.0" // default: localhost
-  },
-
-  /*
-   ** Customize the progress-bar color
-   */
-  loading: { color: "#fff" },
-
-  /*
-   ** Global CSS
-   */
-  css: [
-    // 'bulma',
-    // '~/assets/global.css',
-    '~/assets/global.styl'
-  ],
-
-  /*
-   ** Plugins to load before mounting the App
-   */
-  plugins: [],
-
-  /*
-   ** Nuxt.js modules
-   */
-  modules: [
-    // Doc: https://github.com/nuxt-community/axios-module#usage
-    "@nuxtjs/axios"
-  ],
-  /*
-   ** Axios module configuration
-   */
-  axios: {
-    // See https://github.com/nuxt-community/axios-module#options
-  },
-
-  /*
-   ** Build configuration
-   */
-  build: {
     /*
-     ** You can extend webpack config here
+     ** Headers of the page
      */
-    extend(config, ctx) {}
-  }
+    head: {
+        htmlAttrs: {
+            lang: "en"
+        },
+        title: pkg.name,
+        meta: [
+            { charset: "utf-8" },
+            {
+                name: "viewport",
+                content: "width=device-width, initial-scale=1"
+            },
+            {
+                hid: "description",
+                name: "description",
+                content: pkg.description
+            }
+        ],
+        link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }]
+    },
+
+    server: {
+        port: process.env.PORT || 8000, // default: 3000
+        host: "0.0.0.0" // default: localhost
+    },
+
+    /*
+     ** Customize the progress-bar color
+     */
+    loading: { color: "#fff" },
+
+    /*
+     ** Global CSS
+     */
+    css: [
+        // 'bulma',
+        // '~/assets/global.css',
+        "~/assets/global.styl"
+    ],
+
+    /*
+     ** Plugins to load before mounting the App
+     */
+    plugins: [],
+
+    /*
+     ** Nuxt.js modules
+     */
+    modules: [
+        // Doc: https://github.com/nuxt-community/axios-module#usage
+        "@nuxtjs/axios"
+    ],
+    /*
+     ** Axios module configuration
+     */
+    axios: {
+        // See https://github.com/nuxt-community/axios-module#options
+    },
+
+    /*
+     ** Build configuration
+     */
+    build: {
+        /*
+         ** You can extend webpack config here
+         */
+        extend(config, ctx) {}
+    }
 };
