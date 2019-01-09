e1d9ae5f45355c69d7136c35ada2b98e9dd7179c
unclecode <unclecode@kidocode.com>
2019-01-09 22:48:50 +0800

    done

--- components/Logo.vue
+++ components/Logo.vue
@@ -1,5 +1,5 @@
 <template>
-  <img src="https://s3-ap-southeast-1.amazonaws.com/kidocodeweb/assets/images/KIDOCODE_LOGO_2.svg">
+  <img src="https://s3-ap-southeast-1.amazonaws.com/kidocodeweb/assets/images/KIDOCODE_LOGO_2.svg" alt="kidocode">
 </template> 
 
 
